6cbb5bb9d3d55f44d602b862c4beaadc41d04912
xuanyuan <xuanyuanshenfu@outlook.com>
2023-02-20 14:49:37 +0800

    init

--- Package.swift
+++ Package.swift
@@ -6,21 +6,22 @@ import PackageDescription
 let package = Package(
     name: "c2sp",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "c2sp",
             targets: ["c2sp"]),
+        .library(
+            name: "CHalf",
+            targets: ["CHalf"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "CHalf"),
         .target(
             name: "c2sp",
-            dependencies: []),
+            dependencies: ["CHalf"]),
         .testTarget(
             name: "c2spTests",
             dependencies: ["c2sp"]),
--- Sources/c2sp/SysInfo.swift
+++ Sources/c2sp/SysInfo.swift
@@ -5,10 +5,18 @@
 //  Created by xuanyuan on 2023/2/20.
 //
 
+#if SWIFT_PACKAGE
+import CHalf
+#endif
+
 import Foundation
 
 open class SysInfo {
     public static func getMomory() ->String{
-        return "01 MB"
+//        return "01 MB"
+        
+        let zh = test()
+        print(zh)
+        return "\(zh) MB"
     }
 }
