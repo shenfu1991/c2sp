9b9fe83486b12c2c7309d6d8ecb51a1ea7c1d182
xuanyuan <xuanyuanshenfu@outlook.com>
2023-02-20 14:35:52 +0800

    add

---
+++ Sources/c2sp/SysInfo.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  
+//
+//  Created by xuanyuan on 2023/2/20.
+//
+
+import Foundation
+
+open class SysInfo {
+    static func getMomory() {
+        return "01 MB"
+    }
+}
